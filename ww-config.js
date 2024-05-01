b6eecc75c039ef8c9cb41b41b7ee2c89a561bab2
zeeshan <zeeshanweb.official@gmail.com>
2024-05-01 07:29:00 +0500

    first commit

--- ww-config.js
+++ ww-config.js
@@ -1,10 +1,27 @@
 export default {
   editor: {
     label: {
-      en: "My Element",
+      en: "Highcharts Data Visualization",
     },
   },
   properties: {
+    title: {
+      label: {
+        en: 'title',
+        fr: 'Texte par défaut',
+      },
+      type: 'Text',
+      defaultValue: { en: 'title' },
+      section: 'settings',
+      bindable: true,
+      multiLang: true,
+      /* wwEditor:start */
+      bindingValidation: {
+        type: 'string',
+        tooltip: 'A string that defines the title: `"title"`',
+      },
+      /* wwEditor:end */
+    },
     data: {
       label: {
         en: "Grid data",
@@ -14,85 +31,7 @@ export default {
         text: { en: "Bind your data" },
       },
       bindable: "repeatable",
-      defaultValue: [
-        {
-          chart: {
-            type: "column",
-            options3d: {
-              enabled: true,
-              alpha: 15,
-              beta: 15,
-              viewDistance: 25,
-              depth: 40,
-            },
-          },
 
-          title: {
-            text: " Electricity production in countries, grouped by continent",
-            align: "left",
-          },
-
-          xAxis: {
-            labels: {
-              skew3d: true,
-              style: {
-                fontSize: "16px",
-              },
-            },
-          },
-
-          yAxis: {
-            allowDecimals: false,
-            min: 0,
-            title: {
-              text: "TWh",
-              skew3d: true,
-              style: {
-                fontSize: "16px",
-              },
-            },
-          },
-
-          tooltip: {
-            headerFormat: "<b>{point.key}</b><br>",
-            pointFormat:
-              '<span style="color:{series.color}">\u25CF</span> {series.name}: {point.y} / {point.stackTotal}',
-          },
-
-          plotOptions: {
-            series: {
-              pointStart: 2016,
-            },
-            column: {
-              stacking: "normal",
-              depth: 40,
-            },
-          },
-
-          series: [
-            {
-              name: "South Korea",
-              data: [563, 567, 590, 582, 571],
-              stack: "Asia",
-            },
-            {
-              name: "Germany",
-              data: [650, 654, 643, 612, 572],
-              stack: "Europe",
-            },
-            {
-              name: "Saudi Arabia",
-              data: [368, 378, 378, 367, 363],
-              stack: "Asia",
-            },
-            {
-              name: "France",
-              data: [564, 562, 582, 571, 533],
-              stack: "Europe",
-            },
-          ],
-        },
-      ],
       section: "settings",
       /* wwEditor:start */
       bindingValidation: {
@@ -109,90 +48,6 @@ export default {
       },
       /* wwEditor:end */
     },
-    // chartData: {
-    //   label: {
-    //     en: "Chart data",
-    //   },
-    //   type: "Object",
-    //   defaultValue: {
-    //     chart3d: {
-    //       chart: {
-    //         type: "column",
-    //         options3d: {
-    //           enabled: true,
-    //           alpha: 15,
-    //           beta: 15,
-    //           viewDistance: 25,
-    //           depth: 40,
-    //         },
-    //       },
 
-    //       title: {
-    //         text: " Electricity production in countries, grouped by continent",
-    //         align: "left",
-    //       },
-
-    //       xAxis: {
-    //         labels: {
-    //           skew3d: true,
-    //           style: {
-    //             fontSize: "16px",
-    //           },
-    //         },
-    //       },
-
-    //       yAxis: {
-    //         allowDecimals: false,
-    //         min: 0,
-    //         title: {
-    //           text: "TWh",
-    //           skew3d: true,
-    //           style: {
-    //             fontSize: "16px",
-    //           },
-    //         },
-    //       },
-
-    //       tooltip: {
-    //         headerFormat: "<b>{point.key}</b><br>",
-    //         pointFormat:
-    //           '<span style="color:{series.color}">\u25CF</span> {series.name}: {point.y} / {point.stackTotal}',
-    //       },
-
-    //       plotOptions: {
-    //         series: {
-    //           pointStart: 2016,
-    //         },
-    //         column: {
-    //           stacking: "normal",
-    //           depth: 40,
-    //         },
-    //       },
-
-    //       series: [
-    //         {
-    //           name: "South Korea",
-    //           data: [563, 567, 590, 582, 571],
-    //           stack: "Asia",
-    //         },
-    //         {
-    //           name: "Germany",
-    //           data: [650, 654, 643, 612, 572],
-    //           stack: "Europe",
-    //         },
-    //         {
-    //           name: "Saudi Arabia",
-    //           data: [368, 378, 378, 367, 363],
-    //           stack: "Asia",
-    //         },
-    //         {
-    //           name: "France",
-    //           data: [564, 562, 582, 571, 533],
-    //           stack: "Europe",
-    //         },
-    //       ],
-    //     },
-    //   },
-    // },
   },
 };
